3969a905ca194884812dc90a92ef9896ad765b0e
web-dev-manish <190101120091@cutm.ac.in>
2021-07-14 14:00:58 +0530

    My first react Hello World Web App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDOM from "react"
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="hello">
+            <h1>
+                Hello World!
+            </h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, doloribus nostrum. Deserunt, aut. Porro enim tempora harum magnam at id in, sunt quas? Nostrum eum, adipisci commodi aspernatur laudantium quas.</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
